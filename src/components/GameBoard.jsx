f0dbc055453f46436bcb3a0deec090db17f5690e
Olga Felczak <ofelczak@gmail.com>
2023-12-08 16:04:41 +0000

    Refactor GameBoard component to use turns (Deriving State From Props)

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,21 +4,15 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare }) {
-  // Avoid Intersecting States
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-  //   onSelectSquare();
-  // }
+    gameBoard[row][col] = player;
+  }
 
   return (
     <ol id="game-board">
@@ -27,7 +21,9 @@ export default function GameBoard({ onSelectSquare }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
